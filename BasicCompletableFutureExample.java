130a44867d8866d167e2039ca4919be6fd650adc
GitSarita <mahato.sarita173@gmail.com>
2019-01-22 11:38:03 +0530

    Create BasicCompletableFutureExample.java

---
+++ BasicCompletableFutureExample.java
@@ -0,0 +1,5 @@
+package futureexamples;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
